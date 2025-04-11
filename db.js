62a76e4ffadd28fbc0cfd4faf36f4d2c3c19aef1
Pavankarthikeya08 <your-email@example.com>
2025-04-11 23:21:14 +0530

    db changed

--- db.js
+++ db.js
@@ -1,4 +1,25 @@
 const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
+
+
+
+/*const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(`mongodb://localhost:27017/school`, {
@@ -14,3 +35,4 @@ const connectDB = async () => {
   }
 };
 module.exports = connectDB;
+*/
