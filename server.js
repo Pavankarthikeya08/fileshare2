24606e1147212d0443b6867ae3825a5c7ac76097
Pavankarthikeya08 <your-email@example.com>
2025-04-11 22:47:02 +0530

    port added

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ const connectDB=require('./db.js');
 connectDB();
 app.use("/",router1);
 app.use("/",router2);
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
