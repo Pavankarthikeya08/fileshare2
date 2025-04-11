a8bd57ba981a4fa905dcc932c71e9e6c61e89783
Pavankarthikeya08 <your-email@example.com>
2025-04-11 23:23:44 +0530

    cloud changed

--- cloud.js
+++ cloud.js
@@ -1,4 +1,15 @@
-const cloudinary=new require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
+module.exports = cloudinary;
+
+
+/*const cloudinary=new require('cloudinary').v2;
 cloudinary.config({
     cloud_name: "dxbcetv7j",    
   api_key: "481137744182885",            
@@ -7,3 +18,5 @@ cloudinary.config({
 
 
 module.exports=cloudinary;
+
+*/
